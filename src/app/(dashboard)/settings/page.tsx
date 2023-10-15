24aa392b7a0ea198829e4b1e8148b1768d67d35e
samueloh99 <samueloh99@gmail.com>
2023-10-15 03:44:09 -0300

    Created heading component

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -1,3 +1,9 @@
+import { Headings } from "@/components/headings";
+
 export default function SettingsPage() {
-  return <div>dashboard page</div>;
+  return (
+    <div className="flex-1 space-y-4 p-8 pt-6 border border-black">
+      <Headings description="Manage store preferences" title="Settings" />
+    </div>
+  );
 }
